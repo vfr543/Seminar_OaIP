0f6e8569718fa69ab6f5c032a6ec16a59d683295
vfr543 <127236384+vfr543@users.noreply.github.com>
2025-10-05 16:27:05 +0300

    Task9geo.cpp

--- 3rd_seminar/Task9geo.cpp
+++ 3rd_seminar/Task9geo.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-double vectPr(int x1, int y1, int x2, int y2, int x3, int y3) {
+int vectPr(int x1, int y1, int x2, int y2, int x3, int y3) {
 	int pr = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
 	return pr;
+}
+int min(int x1, int x2) {
+	if (x1 < x2) {
+		return x1;
+	}
+	else {
+		return x2;
+	}
+}
+int max(int x1, int x2) {
+	if (x1 > x2) {
+		return x1;
+	}
+	else {
+		return x2;
+	}
+}
+bool onLine(int x1, int y1, int x2, int y2, int x3, int y3) {
+	if (min(x1, x2) <= x3 && x3 <= max(x1, x2) && min(y1, y2) <= y3 && y3 <= max(y1, y2)) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 int main() {
 	setlocale(LC_ALL, "Russian");
@@ -12,56 +36,57 @@ int main() {
 	vector<int> arrx2;
 	vector<int> arry2;
 	int n1, n2;
-	int flag1 = 0, flag2 = 0;
-	cout << "Ââåäèòå êîëè÷åñòâî âåðøèí: ";
+	bool temp = false;
+	cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ ÐºÐ¾Ð»Ð¸Ñ‡ÐµÑÑ‚Ð²Ð¾ Ð²ÐµÑ€ÑˆÐ¸Ð½: ";
 	cin >> n1;
 	int value1, value2;
 	for (int i = 0; i < n1; i++) {
-		cout << "Ââåäèòå êîîðäèíàòû x âåðøèíû " << i + 1 << " : ";
+		cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ ÐºÐ¾Ð¾Ñ€Ð´Ð¸Ð½Ð°Ñ‚Ñ‹ x Ð²ÐµÑ€ÑˆÐ¸Ð½Ñ‹ " << i + 1 << " : ";
 		cin >> value1;
-		cout << "Ââåäèòå êîîðäèíàòû y âåðøèíû " << i + 1 << " : ";
+		cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ ÐºÐ¾Ð¾Ñ€Ð´Ð¸Ð½Ð°Ñ‚Ñ‹ y Ð²ÐµÑ€ÑˆÐ¸Ð½Ñ‹ " << i + 1 << " : ";
 		cin >> value2;
 		arrx1.push_back(value1);
 		arry1.push_back(value2);
 	}
-	cout << "Ââåäèòå êîëè÷åñòâî âåðøèí: ";
+	cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ ÐºÐ¾Ð»Ð¸Ñ‡ÐµÑÑ‚Ð²Ð¾ Ð²ÐµÑ€ÑˆÐ¸Ð½: ";
 	cin >> n2;
 	for (int i = 0; i < n2; i++) {
-		cout << "Ââåäèòå êîîðäèíàòû x âåðøèíû " << i + 1 << " : ";
+		cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ ÐºÐ¾Ð¾Ñ€Ð´Ð¸Ð½Ð°Ñ‚Ñ‹ x Ð²ÐµÑ€ÑˆÐ¸Ð½Ñ‹ " << i + 1 << " : ";
 		cin >> value1;
-		cout << "Ââåäèòå êîîðäèíàòû y âåðøèíû " << i + 1 << " : ";
+		cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ ÐºÐ¾Ð¾Ñ€Ð´Ð¸Ð½Ð°Ñ‚Ñ‹ y Ð²ÐµÑ€ÑˆÐ¸Ð½Ñ‹ " << i + 1 << " : ";
 		cin >> value2;
 		arrx2.push_back(value1);
 		arry2.push_back(value2);
 	}
+	arrx1.push_back(arrx1[0]);
+	arry1.push_back(arry1[0]);
+	arrx2.push_back(arrx2[0]);
+	arry2.push_back(arry2[0]);
 	for (int j = 0; j < n2; j++) {
-		for (int i = 0; i < n1 - 1; i++) {
-			int temp = vectPr(arrx1[i], arry1[i], arrx1[i + 1], arry1[i + 1], arrx2[j], arry2[j]);
-			if (flag1 == 0 && temp > 0) {
-				flag1 = 0;
+		for (int i = 0; i < n1; i++) {
+			int d1 = vectPr(arrx1[i], arry1[i], arrx1[i + 1], arry1[i + 1], arrx2[j], arry2[j]);
+			int d2 = vectPr(arrx1[i], arry1[i], arrx1[i + 1], arry1[i + 1], arrx2[j + 1], arry2[j + 1]);
+			int d3 = vectPr(arrx2[j], arry2[j], arrx2[j + 1], arry2[j + 1], arrx1[i], arry1[i]);
+			int d4 = vectPr(arrx2[j], arry2[j], arrx2[j + 1], arry2[j + 1], arrx1[i + 1], arry1[i + 1]);
+			if (((d1 < 0 && d2 > 0) || (d1 > 0 && d2 < 0)) && ((d3 < 0 && d4 > 0) || (d3 > 0 && d4 < 0))) {
+				temp = true;
 			}
-			else if (temp == 0) {
-				flag1 = 1;
-			}
-			else {
-				flag1 = 2;
+			else if ((d1 == 0 && onLine(arrx1[i], arry1[i], arrx1[i + 1], arry1[i + 1], arrx2[j], arry2[j])) ||
+					(d2 == 0 && onLine(arrx1[i], arry1[i], arrx1[i + 1], arry1[i + 1], arrx2[j + 1], arry2[j + 1])) ||
+					(d3 == 0 && onLine(arrx2[j], arry2[j], arrx2[j + 1], arry2[j + 1], arrx1[i], arry1[i])) ||
+					(d4 == 0 && onLine(arrx2[j], arry2[j], arrx2[j + 1], arry2[j + 1], arrx1[i + 1], arry1[i + 1]))) {
+				temp = true;
 			}
 		}
-		if (flag1 == 0 || flag1 == 1) {
-			flag2 = 0;
+		if (temp) {
 			break;
 		}
-		else {
-			flag1 = 0;
-		}
 	}
-	switch (flag2) {
-	case 0:
-		cout << "Ìíîãîãðàííèêè ïåðåñåêàþòñÿ";
-		break;
-	case 1:
-		cout << "Ìíîãîãðàííèêè íå ïåðåñåêàþòñÿ";
-		break;
+	if (temp) {
+		cout << "ÐœÐ½Ð¾Ð³Ð¾Ð³Ñ€Ð°Ð½Ð½Ð¸ÐºÐ¸ Ð¿ÐµÑ€ÐµÑÐµÐºÐ°ÑŽÑ‚ÑÑ";
+	}
+	else {
+		cout << "ÐœÐ½Ð¾Ð³Ð¾Ð³Ñ€Ð°Ð½Ð½Ð¸ÐºÐ¸ Ð½Ðµ Ð¿ÐµÑ€ÐµÑÐµÐºÐ°ÑŽÑ‚ÑÑ";
 	}
 	return 0;
 }
