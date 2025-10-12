28f854aa0ca5c29c8993d33e4910e6c56c71b214
vfr543 <127236384+vfr543@users.noreply.github.com>
2025-10-12 21:37:07 +0300

    Task_6.cpp

--- 4th_seminar/Task_6.cpp
+++ 4th_seminar/Task_6.cpp
@@ -5,7 +5,7 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	int a, matrix[2][3];
 	double det, dx, dy, x, y;
-	cout << "Ââåäèòå ìàòðèöó: " << endl;
+	cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ Ð¼Ð°Ñ‚Ñ€Ð¸Ñ†Ñƒ: " << endl;
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < 3; j++) {
 			cin >> a;
